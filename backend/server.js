21910fb3f4cab830c4d3de5899f935f408fa3d7b
vaibhav <vaibhavsachdeva2000@gmail.com>
2022-08-04 23:38:13 +0530

    mongodb atlas connection

--- backend/server.js
+++ backend/server.js
@@ -1,28 +1,25 @@
-const express  = require('express');
-const dotenv  = require('dotenv');
-const {chats} = require("./data/data")
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
 
-const app  = express();
+const app = express();
 dotenv.config();
+connectDB();
 
+app.get("/", (req, res) => {
+  res.send("API is running sucessfully");
+});
 
-app.get('/',(req,res)=>
-{
-    res.send("API is running sucessfully")
-})
-
-app.get('/api/chat',(req,res)=>{
+app.get("/api/chat", (req, res) => {
   res.send(chats);
-})
+});
 
-app.get('/api/chat/:id',(req,res)=>
-{
-    const singlechat  = chats.find((c)=> c._id === req.params.id);
-    res.send(singlechat);
-})
+app.get("/api/chat/:id", (req, res) => {
+  const singlechat = chats.find((c) => c._id === req.params.id);
+  res.send(singlechat);
+});
 
 const PORT = process.env.PORT || 5000;
 
-
-app.listen(5000,console.log(`Server Started on port ${PORT}`));
-
+app.listen(5000, console.log(`Server Started on port ${PORT}`));
